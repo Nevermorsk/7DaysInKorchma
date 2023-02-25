e1670368814f1f68ffa110b639079e9c24e4c57f
Sebsish <llki11er228ll@gmail.com>
2023-02-25 22:50:47 +0700

    Make base for audio

--- Assets/Scrips/UI/MainMenuButtonsScript.cs
+++ Assets/Scrips/UI/MainMenuButtonsScript.cs
@@ -1,24 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenuButtonsScript : MonoBehaviour
 {
-    public Button start;
-    public Button exit;
-    public Button settings;
+    public AudioMixerGroup musicMixer;
+    public AudioMixerGroup effectsMixer;
+    public AudioMixerGroup dilogueMixer;
 
     void Start()
     {
-        start.onClick.AddListener(() => {
-            SceneManager.LoadScene("Game");
-        });
-        exit.onClick.AddListener(() => {
-            Application.Quit();        });
-        settings.onClick.AddListener(() => {
-            SceneManager.LoadScene("Settings");
-        });
+
     }
+    public void StartClc()
+    {
+        SceneManager.LoadScene("Game");
+    }    
+    public void SettingsClc()
+    {
+        SceneManager.LoadScene("Settings");
+    }    
+    public void QuitClc()
+    {
+        Application.Quit();
+    }
+
 }
--- Assets/Scrips/UI/SettingsScript.cs
+++ Assets/Scrips/UI/SettingsScript.cs
@@ -1,17 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
 
 public class SettingsScript : MonoBehaviour
 {
-    public Button exit;
-
+    public Slider musicVolume;
+    public Slider effectsVolume;
+    public Slider dialogueVolume;
+    public AudioMixerGroup musicMixer;
+    public AudioMixerGroup effectsMixer;
+    public AudioMixerGroup dilogueMixer;
     void Start()
     {
-        exit.onClick.AddListener(() => {
-            SceneManager.LoadScene("MainMenu");
-        });
+        Debug.Log($"{PlayerPrefs.GetFloat("musicVolume")} {PlayerPrefs.GetFloat("effectVolume")} {PlayerPrefs.GetFloat("dialogueVolume")}");
+        musicVolume.value = PlayerPrefs.GetFloat("musicVolume");
+        effectsVolume.value = PlayerPrefs.GetFloat("effectVolume");
+        dialogueVolume.value = PlayerPrefs.GetFloat("dialogueVolume");
+/*        effectsMixer.audioMixer.SetFloat("EffectsVolume", effectsVolume.value);
+        musicMixer.audioMixer.SetFloat("MusicVolume", musicVolume.value);
+        dilogueMixer.audioMixer.SetFloat("DialogueVolume", dialogueVolume.value);*/
+    }
+    public void exitClc()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void ChangeEffectsVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("effectVolume", effectsVolume.value);
+        effectsMixer.audioMixer.SetFloat("EffectsVolume", volume);
+    }    
+    public void ChangeMusicVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("musicVolume", musicVolume.value);
+        musicMixer.audioMixer.SetFloat("MusicVolume", volume);
+    }    
+    public void ChangeDialogueVolume(float volume)
+    {
+        PlayerPrefs.SetFloat("dialogueVolume", dialogueVolume.value);
+        dilogueMixer.audioMixer.SetFloat("DialogueVolume", volume);
     }
 }
